3bf36fd252673e055f607244d2ffa16b3af4ca6c
Yusuke Arai <arai@zamec.org>
2018-07-18 11:25:02 +0900

    Update emacs init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,6 @@
+;; Emacs全体の設定
+;; --------------------------------------------------------------
+
 ; メニューバーを消す
 (menu-bar-mode 0)
 
@@ -10,62 +13,60 @@
 ; タブはスペースに展開する
 (setq-default indent-tabs-mode nil)
 
+; 画面表示をUTF-8にする
+(set-terminal-coding-system 'utf-8)
+; キーボードの入力をUTF-8にする
+(set-keyboard-coding-system 'utf-8)
+; ファイルのバッファのデフォルト文字コードをUTF-8にする
+(set-buffer-file-coding-system 'utf-8)
+; バッファのプロセスの文字コードをUTF-8にする
+(setq default-buffer-file-coding-system 'utf-8)
+; ファイルの文字コードをUTF-8にする
+(setq file-name-coding-system 'utf-8)
+; 新規作成ファイルの文字コードをUTF-8にする
+(set-default-coding-systems 'utf-8)
 
+
+;; パッケージ
+;; --------------------------------------------------------------
 (require 'package)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(company-ghc-show-info t)
- '(haskell-stylish-on-save t)
- '(haskell-tags-on-save t)
- '(package-archives
-   (quote
-    (("gnu" . "http://elpa.gnu.org/packages/")
-     ("melpa" . "https://melpa.org/packages/")))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
-;; 初期化
+(defvar package-list
+  '(intero
+    php-mode
+    ac-php)
+  "Packages to be installed")
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
+(unless package-archive-contents (package-refresh-contents))
+(dolist (pkg package-list)
+  (unless (package-installed-p pkg)
+    (package-install pkg)))
 
 
-;; ==================== Haskell ====================
-;; Required packages are:
-;; * haskell-mode
-;; * ghc
-;; * company-ghc
+;; Haskell
+;; --------------------------------------------------------------
 
-; ghc-mod
-(autoload 'ghc-init "ghc" nil t)
-(autoload 'ghc-debug "ghc" nil t)
+; ファイル保存時にstylish-haskellを実行
+(custom-set-variables '(haskell-stylish-on-save t))
+; Interoをhaskell-modeで常に有効化
+(intero-global-mode 1)
+; hlintを有効化
+(flycheck-add-next-checker 'intero '(warning . haskell-hlint))
+
 ; HaRe
 (add-to-list 'load-path "/home/arai/.stack/snapshots/x86_64-linux-nix/lts-8.23/8.0.2/share/x86_64-linux-ghc-8.0.2/HaRe-0.8.4.0/elisp")(require 'hare)
 (autoload 'hare-init "hare" nil t)
 ; haskell-mode
 (add-hook 'haskell-mode-hook
           (lambda ()
-            (ghc-init)
             (hare-init)
-            (define-key haskell-mode-map (kbd "C-c b") 'haskell-mode-jump-to-def-or-tag)
-            (define-key haskell-mode-map (kbd "C-c t") 'haskell-mode-show-type-at)
             ))
-; company-ghc
-(require 'company)
-(add-hook 'haskell-mode-hook 'company-mode)
-(add-to-list 'company-backends 'company-ghc)
 
 
-;; ==================== PHP ====================
-;; Required packages are:
-;; * php-mode
-;; * ac-php
+;; PHP
+;; --------------------------------------------------------------
 
 ; ac-php
 (add-hook 'php-mode-hook
@@ -83,8 +84,3 @@
             (company-mode t)
             (add-to-list 'company-backends 'company-ac-php-backend )
             ))
-
-;; ==================== ElScreen ====================
-;; Required packages are:
-;; * elscreen
-;(elscreen-start)
