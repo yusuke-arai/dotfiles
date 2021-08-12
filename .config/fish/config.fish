6ec2aa38232a5275f982b206f2d38ca4fb778c7a
ARAI Yusuke <arai@zamec.org>
2021-08-13 08:03:42 +0900

    Create temporary files if running on WSL

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,8 @@
+# Create temporary files if running on WSL
+if [ -n "$WSLENV" ]
+    sudo systemd-tmpfiles --create
+end
+
 # Replace the process to tmux
 if [ -z "$TMUX" -a x"$TERM" != x"screen" ]
     switch "$WSLENV"
