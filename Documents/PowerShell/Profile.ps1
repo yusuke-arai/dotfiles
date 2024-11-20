66de631c0223e523dc560f02095a59de75441445
ARAI Yusuke <arai@minabu.co.jp>
2024-11-20 11:47:51 +0900

    Add a key handler for completion

--- Documents/PowerShell/Profile.ps1
+++ Documents/PowerShell/Profile.ps1
@@ -2,6 +2,7 @@ if ($Host.Name -eq 'ConsoleHost')
 {
   Import-Module PSReadLine
   Set-PSReadLineOption -EditMode Emacs
+  Set-PSReadLineKeyHandler -Chord Tab -Function Complete
   Set-PSReadLineKeyHandler -Key Ctrl+i -Function Complete
   Set-PSReadLineKeyHandler -Key Ctrl+j -Function AcceptLine
   Import-Module Posh-Git
