850ea85b850439fb660018f0a677b3ec248691a6
ARAI Yusuke <arai@minabu.co.jp>
2026-05-18 13:52:05 +0900

    Don't use oh-my-posh with fish.

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,35 +1,41 @@
 function fish_prompt --description 'Write out the prompt'
-    set -l last_status $status
+    set -l last_pipestatus $pipestatus
+    set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
 
+    if not set -q __fish_git_prompt_color_branch
+        set -g __fish_git_prompt_color_branch magenta --bold
+    end
+
+    set -l color_cwd
+    set -l suffix
+    if functions -q fish_is_root_user; and fish_is_root_user
+        if set -q fish_color_cwd_root
+            set color_cwd $fish_color_cwd_root
+        else
+            set color_cwd $fish_color_cwd
+        end
+        set suffix '#'
+    else
+        set color_cwd $fish_color_cwd
+        set suffix '$'
+    end
+
+    # Newline
     echo
 
-    # User
-    set_color $fish_color_user
-    echo -n $USER
-    set_color normal
-
-    echo -n '@'
-
-    # Host
-    set_color $fish_color_host
-    echo -n (prompt_hostname)
-    set_color normal
-
-    echo -n ':'
-
     # PWD
-    set_color $fish_color_cwd
+    set_color $color_cwd
     echo -n (prompt_pwd)
     set_color normal
 
-    __terlar_git_prompt
-    fish_hg_prompt
-    echo
+    printf '%s ' (fish_vcs_prompt)
 
-    if not test $last_status -eq 0
-        set_color $fish_color_error
-    end
-
-    echo -n '> '
+    set -l status_color (set_color $fish_color_status)
+    set -l statusb_color (set_color --bold $fish_color_status)
+    set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
+    echo -n $prompt_status
     set_color normal
+
+    echo
+    echo -n "$suffix "
 end
