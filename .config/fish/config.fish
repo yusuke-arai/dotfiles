6225c51ab724f52dab11108da64848366d15d195
ARAI Yusuke <arai@minabu.co.jp>
2026-05-29 10:11:53 +0900

    Update config.fish.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,22 @@
 # Create temporary files if running on WSL
-if [ -n "$WSLENV" ]
+if test -n "$WSLENV"
     sudo systemd-tmpfiles --create
 end
 
-set -gx VOLTA_HOME "$HOME/.volta"
-set -gx PATH "$VOLTA_HOME/bin" $PATH
-fish_add_path $HOME/.local/bin
-fish_add_path $HOME/.dotnet/tools
-source "$HOME/.cargo/env.fish"
+if test -d "$HOME/.cargo"
+    source "$HOME/.cargo/env.fish"
+end
+
+if test -d "$HOME/.volta"
+    set -gx VOLTA_HOME "$HOME/.volta"
+    fish_add_path "$VOLTA_HOME/bin"
+end
+
+if test -d "$HOME/.dotnet/tools"
+    fish_add_path "$HOME/.dotnet/tools"
+end
+
+if test -d "$HOME/.local/bin"
+    fish_add_path "$HOME/.local/bin"
+end
+
